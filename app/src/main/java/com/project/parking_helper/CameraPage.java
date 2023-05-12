e94df597ef6be46dddeaf9f937d6a5a3b29deff3
HarshAggarwal1 <haggarwal2510@gmail.com>
2023-05-12 13:21:02 +0530

    Commit: Added Many Files

--- app/src/main/java/com/project/parking_helper/CameraPage.java
+++ app/src/main/java/com/project/parking_helper/CameraPage.java
@@ -35,6 +35,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -45,45 +48,32 @@ import java.nio.file.Files;
 import java.util.Collections;
 import java.util.List;
 
-public class CameraPage extends AppCompatActivity implements SurfaceHolder.Callback{
+public class CameraPage extends AppCompatActivity{
 
-    private SurfaceView cameraPreview;
-    private ImageView flashLightButton;
-    private ImageView shutterButton;
-    private CameraDevice cameraDevice;
-    private CameraCaptureSession cameraCaptureSession;
-    private CaptureRequest.Builder previewRequestBuilder;
-    private Handler backgroundHandler;
-    private boolean isFlashOn = false;
-
+    private CodeScanner mCodeScanner;
+    private final String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+    private final int requestCode = 1;
+    private boolean permissionsGranted = true;
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode,permissions,grantResults);
-        if (requestCode == 1) {
+        if (requestCode == this.requestCode) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 if (ContextCompat.checkSelfPermission(CameraPage.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                     Log.d("MainActivity", "Camera Permission Accepted");
-                }
-            } else {
-                Log.d("MainActivity", "Camera Permission Denied");
-            }
-        }
-        if (requestCode == 2) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (ContextCompat.checkSelfPermission(CameraPage.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                } else if (ContextCompat.checkSelfPermission(CameraPage.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("MainActivity", "Storage Permission Accepted");
+                } else if (ContextCompat.checkSelfPermission(CameraPage.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                     Log.d("MainActivity", "Storage Permission Accepted");
                 }
             } else {
-                Log.d("MainActivity", "Storage Permission Denied");
-            }
-        }
-        if (requestCode == 3) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (ContextCompat.checkSelfPermission(CameraPage.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                    Log.d("MainActivity", "Storage Permission Accepted");
+                if (ContextCompat.checkSelfPermission(CameraPage.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    Log.d("MainActivity", "Camera Permission Denied");
+                } else if (ContextCompat.checkSelfPermission(CameraPage.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    Log.d("MainActivity", "Write Permission Denied");
+                } else if (ContextCompat.checkSelfPermission(CameraPage.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    Log.d("MainActivity", "Read Permission Denied");
                 }
-            } else {
-                Log.d("MainActivity", "Storage Permission Denied");
             }
         }
     }
@@ -93,284 +83,62 @@ public class CameraPage extends AppCompatActivity implements SurfaceHolder.Callb
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        // ---------- Camera Permission ------------
-        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA},1);
-        }
-        else {
-            Log.d("CameraPage", "Camera Permission Already Granted");
-        }
-
-        // ---------- Storage Permission ------------
-        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            System.out.println("Storage Permission Not Granted");
-            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},2);
-        }
-        else {
-            Log.d("CameraPage", "Storage Permission Already Granted");
+        for (String permission : permissions) {
+            if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionsGranted = false;
+                requestPermissions(permissions, requestCode);
+                break;
+            }
         }
 
-        // ---------- Storage Permission ------------
-        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            System.out.println("Storage Permission Not Granted");
-            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},3);
+        if (permissionsGranted) {
+            Log.d("CameraPage", "All Permissions Already Granted");
         }
         else {
-            Log.d("CameraPage", "Storage Permission Already Granted");
+            Log.d("CameraPage", "Permissions Not Granted");
+            finish();
         }
 
-        cameraPreview = findViewById(R.id.camera_preview);
-        if (cameraPreview != null) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                Log.d("MainActivity", "Camera Permission Denied");
-                turnOffFlashLight();
+        CodeScannerView scannerView = findViewById(R.id.camera_preview);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(result -> {
+            runOnUiThread(() -> {
+                Toast.makeText(CameraPage.this, result.getText(), Toast.LENGTH_SHORT).show();
                 finish();
-            }
-
-            // set surface holder
-            cameraPreview.getHolder().setFixedSize(480, 480);
-
-            cameraPreview.getHolder().addCallback(this);
-
-        }
-
-        // ---------- Flashlight Button ------------
-        flashLightButton = findViewById(R.id.camera_flash_button);
-        flashLightButton.setImageResource(R.drawable.flash_off);
-        flashLightButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isFlashOn) {
-                    turnOffFlashLight();
-                    flashLightButton.setImageResource(R.drawable.flash_off);
-                    isFlashOn = false;
-                } else {
-                    turnOnFlashLight();
-                    flashLightButton.setImageResource(R.drawable.flash_on);
-                    isFlashOn = true;
-                }
-            }
+            });
         });
+        scannerView.setOnClickListener(view -> mCodeScanner.startPreview());
 
-        // ---------- Back Button ------------
         ImageView backButton = findViewById(R.id.camera_back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                turnOffFlashLight();
                 finish();
             }
         });
-
-        // ---------- Shutter Button ------------
-        shutterButton = findViewById(R.id.camera_shutter);
-        shutterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                shutterButton.setImageResource(R.drawable.yellow_camera_shutter);
-                shutterButton.setBackgroundResource(R.drawable.circle_bg_yellow_shutter_clicked);
-                captureImage();
-
-            }
-        });
-
-    }
-
-    private void captureImage() {
-        try {
-            CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-            CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraManager.getCameraIdList()[0]);
-            Size[] jpegSizes = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP).getOutputSizes(ImageFormat.JPEG);
-            int width = 720;
-            int height = 720;
-            if (jpegSizes != null && jpegSizes.length > 0) {
-                width = jpegSizes[0].getWidth();
-                height = jpegSizes[0].getHeight();
-            }
-            ImageReader imageReader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
-            List<Surface> outputSurface = Collections.singletonList(imageReader.getSurface());
-
-            CaptureRequest.Builder captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
-            captureBuilder.addTarget(imageReader.getSurface());
-            captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
-            int sensorOrientation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
-            int rotation = getWindowManager().getDefaultDisplay().getRotation();
-            int jpegOrientation = (sensorOrientation + rotation * 90 + 270) % 360;
-            previewRequestBuilder.set(CaptureRequest.JPEG_ORIENTATION, jpegOrientation);
-
-
-            CameraCaptureSession.CaptureCallback captureCallback = new CameraCaptureSession.CaptureCallback() {
-                @Override
-                public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
-                    super.onCaptureCompleted(session, request, result);
-                    try  {
-                        Image image = imageReader.acquireLatestImage();
-                        System.out.println(image.getWidth() + " " + image.getHeight());
-                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-                        byte[] bytes = new byte[buffer.capacity()];
-                        buffer.get(bytes);
-                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                        bitmap = rotateImage(bitmap);
-                        System.out.println(bitmap.getWidth() + " " + bitmap.getHeight());
-                        image.close();
-                        // reduce size of bitmap
-                        bitmap = Bitmap.createScaledBitmap(bitmap, 720, 720, false);
-                        saveImage(bitmap);
-                    }
-                    catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    shutterButton.setImageResource(R.drawable.camera_shutter);
-                    shutterButton.setBackgroundResource(R.drawable.circle_bg_translucent_black);
-                }
-            };
-
-            cameraDevice.createCaptureSession(outputSurface, new CameraCaptureSession.StateCallback() {
-                @Override
-                public void onConfigured(@NonNull CameraCaptureSession session) {
-                    try {
-                        session.capture(captureBuilder.build(), captureCallback, null);
-                    }
-                    catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-
-                }
-            }, backgroundHandler);
-        }
-        catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void saveImage(Bitmap bitmap) {
-        String fileName = "PH" + System.currentTimeMillis() + ".jpg";
-        String path = Environment.getExternalStorageDirectory().toString() + "/DCIM/Camera/" + fileName;
-        File file = new File(Environment.getExternalStorageDirectory().toString() + "/DCIM/Camera/" + "PH" + System.currentTimeMillis() + ".jpg");
-        System.out.println(file.getAbsolutePath());
-        try {
-            OutputStream outputStream = Files.newOutputStream(file.toPath());
-            // reduce quality and size of image
-            System.out.println(bitmap.getWidth() + " " + bitmap.getHeight());
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream);
-            System.out.println(bitmap.getWidth() + " " + bitmap.getHeight());
-            outputStream.flush();
-            outputStream.close();
-            Toast.makeText(CameraPage.this, "Image Saved", Toast.LENGTH_SHORT).show();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        cameraCaptureSession.close();
-        createCameraPreviewSession();
-    }
-
-    private Bitmap rotateImage(Bitmap bitmap) {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(90);
-        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        try {
-            String cameraId = cameraManager.getCameraIdList()[0];
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                Log.d("MainActivity", "Camera Permission Denied");
-            }
-            cameraManager.openCamera(cameraId, new CameraDevice.StateCallback() {
-                @Override
-                public void onOpened(@NonNull CameraDevice camera) {
-                    cameraDevice = camera;
-                    createCameraPreviewSession();
-                }
-
-                @Override
-                public void onDisconnected(@NonNull CameraDevice camera) {
-                    camera.close();
-                    cameraDevice = null;
-                }
-
-                @Override
-                public void onError(@NonNull CameraDevice camera, int error) {
-                    camera.close();
-                    cameraDevice = null;
-                }
-            }, null);
-        } catch (Exception e) {
-            Log.d("CameraPage", "Camera Error");
-            e.printStackTrace();
-        }
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder,int format,int width,int height) {
-
+    protected void onPause() {
+        super.onPause();
+        mCodeScanner.releaseResources();
     }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
-    private void createCameraPreviewSession() {
-        try {
-            Surface surface = cameraPreview.getHolder().getSurface();
-            previewRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            previewRequestBuilder.addTarget(surface);
-
-            cameraDevice.createCaptureSession(Collections.singletonList(surface), new CameraCaptureSession.StateCallback() {
-                @Override
-                public void onConfigured(@NonNull CameraCaptureSession session) {
-                    cameraCaptureSession = session;
-                    try {
-                        cameraCaptureSession.setRepeatingRequest(previewRequestBuilder.build(), null, backgroundHandler);
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-                    Toast.makeText(CameraPage.this, "Unable to create camera preview session", Toast.LENGTH_SHORT).show();
-                }
-            }, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void turnOnFlashLight() {
-        try {
-            if (cameraDevice != null) {
-                previewRequestBuilder.set(CaptureRequest.FLASH_MODE, CaptureRequest.FLASH_MODE_TORCH);
-                cameraCaptureSession.setRepeatingRequest(previewRequestBuilder.build(), null, backgroundHandler);
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void turnOffFlashLight() {
-        try {
-            if (cameraDevice != null) {
-                previewRequestBuilder.set(CaptureRequest.FLASH_MODE, CaptureRequest.FLASH_MODE_OFF);
-                cameraCaptureSession.setRepeatingRequest(previewRequestBuilder.build(), null, backgroundHandler);
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        turnOffFlashLight();
         finish();
     }
 }
